882b864c25fc98e381ea280ea70b0595719cc592
Taylor Savage <taylorsavage93@gmail.com>
2022-07-06 12:41:30 -0400

    operators exercise 1

---
+++ Exercise1.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//for division create two integer variables called a and b.
+
+
+int a = 17;
+int b = 4;
+int quotient = a / b;
+int remainder = a % b;
+//Console.WriteLine(a + " / " + b + " is " + quotient + " remainder " + remainder);//
+Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+
+
