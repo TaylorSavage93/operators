e790f4d44758ad9a69a8de1ed24d044c3cba31f1
Taylor Savage <taylorsavage93@gmail.com>
2022-07-06 15:32:38 -0400

    fixed addition and subtraction

--- Exercise1.cs
+++ Exercise1.cs
@@ -7,8 +7,19 @@ int a = 17;
 int b = 4;
 int quotient = a / b;
 int remainder = a % b;
+int Add = a + b;
 //Console.WriteLine(a + " / " + b + " is " + quotient + " remainder " + remainder);//
 Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+
+Double x = 10;
+Double y = 30;
+x += y;
+x *= y;
+x -= y;
+x /= y;
+
+Console.WriteLine(x);
 
 
 
